fc8a0119401419be72ffd9efe2c99949b24762b8
erdi-ozturk <erdi.ozturk52@gmail.com>
2023-11-10 11:40:36 +0000

    Test Runner Class added to see screenshot and pdf report when scenario is failed.

---
+++ src/test/java/Runner/_07_TestRunnerExtendReport.java
@@ -0,0 +1,13 @@
+package Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        tags = "@SmokeTest",
+        features = {"src/test/java/FeatureFiles"},
+        glue = {"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+public class _07_TestRunnerExtendReport extends AbstractTestNGCucumberTests {
+}
